dc94f50c70a6fdf7144e21b5bf9848808e54f1e5
ElenaSav <slv97@mail.ru>
2020-05-27 19:22:11 +0300

    First commit

---
+++ js_ex/burger.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+    $('.head_burger').click(function(event) {
+        $('.head_burger,.nav').toggleClass('active');
+        $('body').toggleClass('lock');
+    });
+});
